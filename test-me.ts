88f956911845a8a08cb8f29786b64f69a9fd6780
Alex Crișan <acrisan@altametrics.com>
2025-04-10 09:21:56 +0300

    commit three

--- test-me.ts
+++ test-me.ts
@@ -1,21 +1,25 @@
-import type { UnionToTuple } from 'type-fest'
+import type { UnionToTuple } from "type-fest";
 
-type OrderStatus = 'InCart' |
-    'Pending' |
-    'Confirmed' |
-    'Preparing' |
-    'ReadyForPickup' |
-    'InDelivery' |
-    'Delivered' |
-    'Cancelled'
+type OrderStatus =
+  | "InCart"
+  | "Pending"
+  | "Confirmed"
+  | "Preparing"
+  | "ReadyForPickup"
+  | "InDelivery"
+  | "Delivered"
+  | "Cancelled";
 
-export const willFailWithTsgo10April2025 = [
-    'InCart',
-    'Pending',
-    'Confirmed',
-    'Preparing',
-    'ReadyForPickup',
-    'InDelivery',
-    'Delivered',
-    'Cancelled',
-] as const satisfies UnionToTuple<OrderStatus>
+/**
+ * Fails with TSGO on 10 April 2025 (https://github.com/microsoft/typescript-go/commit/a88fef77f5a7663513a45ca0b1a431102638fbd2)
+ */
+export const thisFails = [
+  "InCart",
+  "Pending",
+  "Confirmed",
+  "Preparing",
+  "ReadyForPickup",
+  "InDelivery",
+  "Delivered",
+  "Cancelled",
+] as const satisfies UnionToTuple<OrderStatus>;

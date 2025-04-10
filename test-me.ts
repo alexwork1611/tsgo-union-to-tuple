4aaf8423c7747c1a14ab7e732713be6d0a15e9e6
Alex Crișan <acrisan@altametrics.com>
2025-04-10 09:26:09 +0300

    commit four

--- test-me.ts
+++ test-me.ts
@@ -11,7 +11,9 @@ type OrderStatus =
   | "Cancelled";
 
 /**
- * Fails with TSGO on 10 April 2025 (https://github.com/microsoft/typescript-go/commit/a88fef77f5a7663513a45ca0b1a431102638fbd2)
+ * Fails with TSGO on 10 April 2025 (tested at commit https://github.com/microsoft/typescript-go/commit/a88fef77f5a7663513a45ca0b1a431102638fbd2)
+ *
+ * It does not fail with TypeScript 5.8.3
  */
 export const thisFails = [
   "InCart",
